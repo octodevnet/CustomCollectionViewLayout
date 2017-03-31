d418a4e747eb89c9ee5f1ab4e1c567362130b4ad
Dmytro Brovkin <dmytro.brovkin@playtech.com>
2017-03-31 14:31:21 +0300

    Initial commit

---
+++ Swift/OCTCustomCollectionViewLayout/OCTCustomCollectionViewLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCTCustomCollectionViewLayout
+//
+//  Created by dmitry.brovkin on 3/31/17.
+//  Copyright © 2017 dmitry.brovkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
